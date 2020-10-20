9e4f6e04c903b51a827c87bfe2f0a674dd0aeefd
Shihab Jamil <sjamil181180@bscse.uiu.ac.bd>
2020-10-20 12:05:39 +0600

    Updates

---
+++ precache-manifest.2423600a51c5272e622dbc637d4dca13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd83ad908345d30ce0b401323676157",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "be680e05c6a1c7fcec6d",
+    "url": "/react-todo/static/css/main.2984f6cb.chunk.css"
+  },
+  {
+    "revision": "057a970fb08688642ea5",
+    "url": "/react-todo/static/js/2.02e9556b.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/react-todo/static/js/2.02e9556b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be680e05c6a1c7fcec6d",
+    "url": "/react-todo/static/js/main.fa5d25f4.chunk.js"
+  },
+  {
+    "revision": "fb0017227e7befd6528b",
+    "url": "/react-todo/static/js/runtime-main.407035a6.js"
+  }
+]);
