411faabc37dcca542c264ddf275549321a20825b
Shihab Jamil <sjamil181180@bscse.uiu.ac.bd>
2020-10-20 11:52:44 +0600

    react-todo-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, forwardRef, useEffect } from "react";
+import "./App.css";
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import { FormControl, Input, InputLabel } from "@material-ui/core";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever'; 
+import FlipMove from "react-flip-move";
 
-function App() {
+const App = forwardRef((prop, ref) => {
+  const [item, setItem] = useState("");
+  const [lists, setList] = useState([]);
+  // const [color, setColor] = useState('')
+
+  const colors = [
+    "#a6c1ee",
+    "#fda085",
+    "#8fd3f4",
+    "#f5576c",
+    "#00f2fe",
+    "#fa71cd",
+    "#f9d423",
+    "#d09693",
+    "#f4d03f",
+    "#c43a30",
+    "#f5d100",
+    "#fc00ff",
+  ];
+
+  function randomColor() {
+    let index = Math.floor(Math.random() * 12);
+    console.log("function has been called")
+    return colors[index];
+  }
+
+  const addToTheLists = () => {
+    setList([...lists, item]);
+    setItem("");
+  };
+
+  function deleteItem(index) {
+    let arr = lists.filter((val) => {
+      return val != lists[index];
+    });
+    setList(arr);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={ref}>
+      <div className="todo-card col-lg-4 col-md-4 col-sm-12 offset-4">
+        <h1>ToDo List</h1>
+        <form>
+          <FormControl className="inputForm">
+            <div className="input-group">
+              <InputLabel htmlFor="my-input" className="inputLabel">
+                Add to the List
+              </InputLabel>
+              <Input
+                className="input"
+                id="my-input"
+                value={item}
+                onChange={(event) => setItem(event.target.value)}
+              />
+              <button
+                className="addButton"
+                disabled={!item}
+                type="submit"
+                onClick={addToTheLists}
+              >
+                <AddToPhotosIcon />
+              </button>
+            </div>
+          </FormControl>
+        </form>
+        <div className="itemList">
+          <FlipMove>
+            {lists.map((list, index) => (
+              <h2
+                key={index}
+                className="singleItem"
+                style={{ backgroundColor: randomColor() }}
+              >
+                {list} <span> </span>
+                <strong
+                  className="deleteButton"
+                  onClick={(event) => deleteItem(index)}
+                >
+                  <DeleteForeverIcon />
+                </strong>
+              </h2>
+            ))}
+          </FlipMove>
+        </div>
+      </div>
     </div>
   );
-}
+});
 
 export default App;
